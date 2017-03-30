cef562cf0dd54d78f5b9a409e80c86e4831b9a64
Disi A <adis@live.cn>
2017-03-30 15:03:10 -0400

    Rewriting RTCClient

--- WebRTC-iOS-Client/WebRTC-iOS-Client/Utility/WebRTC/RTCClient.swift
+++ WebRTC-iOS-Client/WebRTC-iOS-Client/Utility/WebRTC/RTCClient.swift
@@ -7,9 +7,21 @@
 //
 
 import UIKit
+import WebRTC
+
+let RTC_SERVER_URL = "http://localhost:8080"
+let STUN_SERVER_URL = "stun:stun.l.google.com:19302"
+let TURN_SERVER_URL = "https://turn.votebin.com"
 
 class RTCClient {
     
+    var audioMute = false
+    var videoMute = false
     
+    var localVideoTrack: RTCVideoTrack?
+    
+    var localAudioTrack: RTCAudioTrack?
+
+    var localMediaStream: RTCMediaStream?
 
 }

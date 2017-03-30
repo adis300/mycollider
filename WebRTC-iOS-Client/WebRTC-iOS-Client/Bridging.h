e4538f536b67a6405bdabe8541b78436c64975f2
Disi A <adis@live.cn>
2017-03-30 14:03:11 -0400

    Adding framework & bridging to the proj

---
+++ WebRTC-iOS-Client/WebRTC-iOS-Client/Bridging.h
@@ -0,0 +1,15 @@
+//
+//  Bridging.h
+//  WebRTC-iOS-Client
+//
+//  Created by Disi A on 6/7/16.
+//  Copyright © 2016 votebin.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <WebRTC/RTCPeerConnection.h>
+#import <WebRTC/RTCEAGLVideoView.h>
+#import <WebRTC/RTCVideoTrack.h>
+
+// DISI's impl of http request
+// #import "HTTPRequest.h"
---
+++ WebRTC-iOS-Client/WebRTC-iOS-Client/Utility/WebRTC/RTCClient.swift
@@ -0,0 +1,15 @@
+//
+//  RTCClient.swift
+//  WebRTC-iOS-Client
+//
+//  Created by Disi A on 3/30/17.
+//  Copyright © 2017 Innovation. All rights reserved.
+//
+
+import UIKit
+
+class RTCClient {
+    
+    
+
+}
